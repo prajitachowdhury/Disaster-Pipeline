6a3adfab8b1e8384f799abc996697f5d98df1e8f
Prajita Chowdhury <pcprajita@gmail.com>
2022-06-13 11:40:16 -0700

    Update process_data.py

--- workspace/data/process_data.py
+++ workspace/data/process_data.py
@@ -4,13 +4,31 @@ import numpy as np
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+	'''
+	load data from csv files and merge to single pandas dataframe
+	
+	Input: 
+	messages_filepath - filepath to messages_csv file
+	categories_filepath - filepath to categories_csv file
+	
+	Returns:
+	df: dataframe merging categories and merges
+	'''
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df = pd.merge(messages,categories,on='id')
     return df
     
 def clean_data(df):
+    '''
+    clean dataframe to transorm into usable format
     
+    Input:
+    df: Dataframe to be worked upon
+    
+    Returns:
+    df: Transformed Dataframe 
+    '''
     categories = df['categories'].str.split(";",expand=True)
    
     row = categories.loc[0,:]
@@ -44,9 +62,18 @@ def clean_data(df):
     return df 
     
 def save_data(df, database_filename):
+    '''
+    save dataframe into database
     
+    Input: 
+    df - Dataframe to be saved
+    database_filename - name of database
+    
+    Returns:
+    None
+    '''
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('DisasterResponse.db', engine, index=False)
+    df.to_sql('DisasterResponse.db', engine, index=False,if_exists='replace')
     print(engine)
 
 def main():
